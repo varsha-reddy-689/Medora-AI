03c38fdd54a198fbac6daee936a06099274d79f3
Varsha Reddy Arava <aravav1@udayton.edu>
2026-06-09 17:36:56 -0400

    Added Requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='medora_ai',
+    version='0.1.0',
+    author= 'Varsha Reddy Arava',
+    author_email= 'varshareddyarava@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
