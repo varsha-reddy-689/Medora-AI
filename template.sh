73a31ea001fc8e8a86e14ca58e50f56893d33016
Varsha Reddy Arava <aravav1@udayton.edu>
2026-06-08 20:14:28 -0400

    Project Structure

---
+++ template.sh
@@ -0,0 +1,16 @@
+# Creating directory
+mkdir -p src
+mkdir -p research
+
+# Creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trails.ipynb
+touch README.md
+touch requirements.txt
+
+echo "Project structure created successfully!"
